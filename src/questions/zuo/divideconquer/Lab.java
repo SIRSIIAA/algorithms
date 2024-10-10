02bf0edca7724fe750bc925dd209c19ee2baebf2
长生 <themiao@outlook.com>
2024-10-10 17:37:56 +0800

    1. implement reverse-pairs question (divide-conquer)

--- src/questions/zuo/divideconquer/Lab.java
+++ src/questions/zuo/divideconquer/Lab.java
@@ -73,4 +73,80 @@ public class Lab {
         }
         System.arraycopy(heap, 0, arr, left, heap.length);
     }
+
+    /*
+     * ref: https://leetcode.cn/problems/reverse-pairs/description/
+     * get reverse pairs
+     */
+    public static int getReversePairs(int[] arr) {
+        /*
+         * non-core logic
+         * just avoiding int overflow
+         */
+        long[] nums = new long[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            nums[i] = (long) arr[i];
+        }
+        /*
+         * core logic
+         */
+        return reversePairsDivide(nums, 0, arr.length - 1);
+    }
+
+    private static int reversePairsDivide(long[] arr, int left, int right) {
+        if (left == right) {
+            return 0;
+        }
+        var mid = left + ((right - left) >> 1);
+        return reversePairsDivide(arr, left, mid) + reversePairsDivide(arr, mid + 1, right) + reversePairsConquer(arr, left, mid, right);
+    }
+
+    private static int reversePairsConquer(long[] arr, int left, int mid, int right) {
+        var res = 0;
+        var pairs = 0;
+        for (int i = left, j = mid + 1; i < mid + 1; i++) {
+            /*
+             * for each element in the left part (i)
+             * find the first element on the right that is not less than it (j)
+             * all the elements on the right (before j) will make a reverse pair with i;
+             * meanwhile, the variable pairs tally the counts of reverse pairs;
+             */
+            while (j < right + 1 && arr[i] > (arr[j] << 1)) {
+                pairs++;
+                j++;
+            }
+            /*
+             * pairs won't restore to zero, because of EVERY ELEMENT AFTER i ON THE LEFT IS BIGGER THAN i,
+             * THEY WILL GENERATE THE SAME PAIRS AS i AT LEAST.
+             *
+             * add the pairs of i;
+             */
+            res += pairs;
+        }
+        /*
+         * make the whole part orderly
+         */
+        reversePairsMerge(arr, left, mid, right);
+        return res;
+    }
+
+    /*
+     * just a common merge procedure of merge sort, nothing special
+     */
+    private static void reversePairsMerge(long[] arr, int left, int mid, int right) {
+        var l = left;
+        var r = mid + 1;
+        var p = 0;
+        var heap = new long[right - left + 1];
+        while (l < mid + 1 && r < right + 1) {
+            heap[p++] = arr[l] <= arr[r] ? arr[l++] : arr[r++];
+        }
+        while (l < mid + 1) {
+            heap[p++] = arr[l++];
+        }
+        while (r < right + 1) {
+            heap[p++] = arr[r++];
+        }
+        System.arraycopy(heap, 0, arr, left, heap.length);
+    }
 }
--- src/questions/zuo/divideconquer/test/LittleSummationTest.java
+++ src/questions/zuo/divideconquer/test/LittleSummationTest.java
@@ -3,6 +3,7 @@ package questions.zuo.divideconquer.test;
 import org.junit.jupiter.api.Test;
 import questions.zuo.divideconquer.Lab;
 import utils.generator.RandomNumberGenerator;
+import utils.parser.CommonInputParser;
 
 import java.util.Arrays;
 
@@ -10,7 +11,7 @@ public class LittleSummationTest {
     @Test
     public void testLittleSummation() {
         var arr = new int[]{
-            1, 3, 5, 2, 4, 6
+                1, 3, 5, 2, 4, 6
         };
         var arrCopy = Arrays.copyOf(arr, arr.length);
         System.out.println(Lab.getLittleSummation(arr));
@@ -36,5 +37,43 @@ public class LittleSummationTest {
             }
         }
         return sum;
+    }
+
+    @Test
+    public void testReversePairs() {
+        var arr = new int[]{
+                1, 3, 2, 3, 1
+        };
+        var arr2 = new int[]{
+                2, 4, 3, 5, 1
+        };
+        var arr3 = CommonInputParser.parseIntArrayFromString("[2147483647,2147483647,2147483647,2147483647,2147483647,2147483647]");
+        System.out.println(getReversePairs(arr));
+        System.out.println(Lab.getReversePairs(arr));
+        System.out.println(getReversePairs(arr2));
+        System.out.println(Lab.getReversePairs(arr2));
+        System.out.println(getReversePairs(arr3));
+        System.out.println(Lab.getReversePairs(arr3));
+    }
+
+    private static int getReversePairs(int[] arr) {
+        var pairs = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > (arr[j] << 1)) {
+                    pairs++;
+                }
+            }
+        }
+        return pairs;
+    }
+
+    @Test
+    public void reversePairsTest() {
+        var size = RandomNumberGenerator.randomInt(0, 1000);
+        var arr = RandomNumberGenerator.randomIntArray(size, 0, 1000, true);
+        var arrCopy = Arrays.copyOf(arr, arr.length);
+        System.out.println("pairs 0: " + Lab.getReversePairs(arr));
+        System.out.println("pairs 1: " + getReversePairs(arrCopy));
     }
 }
